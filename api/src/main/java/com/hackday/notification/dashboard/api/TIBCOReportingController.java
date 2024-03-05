db70695c05235778cf7c7114a981a87e95b3e9e0
Prashant.Hurria <prashant.hurria@auspost.com.au>
2024-03-05 14:11:56 +1100

    2 Hello World controllers

---
+++ api/src/main/java/com/hackday/notification/dashboard/api/TIBCOReportingController.java
@@ -0,0 +1,18 @@
+package com.hackday.notification.dashboard.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TIBCOReportingController {
+
+    @GetMapping("/tibco-reporter")
+    @ResponseBody
+    public String sayHello(@RequestParam(name = "articleId", required = false, defaultValue = "ABC") String articleId) {
+        return "Hello World";
+    }
+
+}
+
