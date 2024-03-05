cf37224947c1f3376f6a0a07f8cd03fbbdaca08a
Prashant.Hurria <prashant.hurria@auspost.com.au>
2024-03-05 16:03:44 +1100

    End API Contract.

--- api/src/main/java/com/hackday/notification/dashboard/api/NotificationTrackAPIController.java
+++ api/src/main/java/com/hackday/notification/dashboard/api/NotificationTrackAPIController.java
@@ -1,15 +1,25 @@
 package com.hackday.notification.dashboard.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.hackday.notification.dashboard.api.model.NotificationRecord;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class NotificationTrackAPIController {
     @GetMapping("/notification-track-api")
     @ResponseBody
-    public String sayHello(@RequestParam(name = "articleId", required = false, defaultValue = "ABC") String articleId) {
-        return "Hello World";
+    public Map<String, List<NotificationRecord>> report(@PathVariable String articleId) {
+
+        Map<String, List<NotificationRecord>> records = new HashMap<>();
+        records.put("sap",new ArrayList<>());
+        records.put("tnh",new ArrayList<>());
+        records.put("ens",new ArrayList<>());
+        records.put("sfmc",new ArrayList<>());
+        return records;
+
     }
 }
